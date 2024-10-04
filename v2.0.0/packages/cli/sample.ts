b820407536ab54bd92900af835a620f5e42f80d8
Angela <angela@brainhub.pl>
2024-10-04 09:53:03 +0200

    chore: replace eslint&prettier with biome

--- v2.0.0/packages/cli/sample.ts
+++ v2.0.0/packages/cli/sample.ts
@@ -1,5 +1,32 @@
-// a file to test out TS&Eslint configs
-let a = 1;
-a.toLocaleLowerCase();
+// A file to test out TS&Biome configs
+import * as fs from "fs";
+import { join } from "path";
+import { readFile } from "fs"; // Unused import
 
-const x = [1, 2, 3];
+function tooComplex() {
+	for (let x = 0; x < 10; x++) {
+		for (let y = 0; y < 10; y++) {
+			for (let z = 0; z < 10; z++) {
+				if (x % 2 === 0) {
+					if (y % 2 === 0) {
+						console.log(x > y ? `${x} > ${y}` : `${y} > ${x}`);
+					}
+				}
+			}
+		}
+	}
+}
+
+const boolExp = true;
+const r = true && boolExp;
+
+export default tooComplex;
+let a_value = 0;
+
+const a = async () => {
+	a_value = await Promise.resolve(1);
+};
+
+a();
+
+console.log(a_value);
