a3e6be75ab5ce711442e5470fcdf97585cad348d
Filip Kublin <filip.kublin@brainhub.pl>
2024-10-09 15:11:45 +0200

    feat(cli): add minimal Commander and Pastel implementations
    BREAKING CHANGE: introduces changes to package.json and tsconfig

---
+++ packages/cli/src/commander/commander.ts
@@ -0,0 +1,16 @@
+import { Command } from "@commander-js/extra-typings";
+import { handler } from "./handler.js";
+
+const program = new Command();
+
+program.name("commander-poc").description("Commander proof of concept");
+
+program
+  .command("do-something")
+  .description("Split a string into substrings and display as an array")
+  .argument("<string>", "string to split")
+  .option("--first", "display just the first substring")
+  .option("-s, --separator <char>", "separator character", ",")
+  .action(handler);
+
+program.parse();
