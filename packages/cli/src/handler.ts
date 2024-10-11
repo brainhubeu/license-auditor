3723022785eca3755fa90dfac4a8a20df15c8462
Filip Kublin <filip.kublin@brainhub.pl>
2024-10-11 12:58:03 +0200

    feat(cli, pastel-cli): create CLI POCs

---
+++ packages/cli/src/handler.ts
@@ -0,0 +1,15 @@
+const handler = (
+  str: string,
+  options: { first?: true | undefined; separator: string }
+) => {
+  const limit = options.first ? 1 : undefined;
+  const output = str
+    .split(options.separator, limit)
+    .reduce(
+      (acc: Record<string, number>, curr, index) => ((acc[curr] = index), acc),
+      {}
+    );
+  console.log(output);
+};
+
+export { handler };
---
+++ packages/pastel-cli/src/commands/index.tsx
@@ -0,0 +1,20 @@
+
+import { Text } from "ink";
+import React from "react";
+import zod from "zod";
+
+export const options = zod.object({
+  name: zod.string().default("Stranger").describe("Name"),
+});
+
+type Props = {
+  options: zod.infer<typeof options>;
+};
+
+export default function Index({ options }: Props) {
+  return (
+    <Text>
+      Hello, <Text color="green">{options.name}</Text>
+    </Text>
+  );
+}
