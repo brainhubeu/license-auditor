55c2e92a605598c87f2206c20f31112b729d3ce3
Angela <angela@brainhub.pl>
2024-10-11 16:04:58 +0200

    fix(core): handle package names with '/'

--- packages/core/src/file-utils.ts
+++ packages/core/src/file-utils.ts
@@ -11,6 +11,14 @@ export function readPackageJson(packagePath: string) {
   console.warn(`package.json not found for package at ${packageJsonPath}`);
 }
 
+// done this way to avoid reading package.json when checking for an existing value in Map
+// if it proves unreliable reading package.json will be inevitable
 export function extractPackageName(packagePath: string) {
-  return path.basename(packagePath);
+  const baseName = path.basename(packagePath);
+  const parentName = path.basename(path.dirname(packagePath));
+
+  if (parentName.startsWith("@")) {
+    return `${parentName}/${baseName}`;
+  }
+  return baseName;
 }
