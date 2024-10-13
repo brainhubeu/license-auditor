ce4572a1ed981a83387f28f8f30d8a5bdb72472d
Filip Kublin <filip.kublin@brainhub.pl>
2024-10-13 12:43:23 +0200

    refactor(biome): update biome config

--- packages/cli/src/handler.ts
+++ packages/cli/src/handler.ts
@@ -3,12 +3,7 @@ const handler = (
   options: { first?: true | undefined; separator: string },
 ) => {
   const limit = options.first ? 1 : undefined;
-  const output = str
-    .split(options.separator, limit)
-    .reduce(
-      (acc: Record<string, number>, curr, index) => ((acc[curr] = index), acc),
-      {},
-    );
+  const output = str.split(options.separator, limit);
   console.log(output);
 };
 
