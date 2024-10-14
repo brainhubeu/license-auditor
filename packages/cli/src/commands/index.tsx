81cbcb33f0d2c5f6e3dd59190e29569bea36c337
F-Kublin <135110213+F-Kublin@users.noreply.github.com>
2024-10-14 14:25:46 +0200

    feat: create CLI POCs (#48)
    * feat(cli, pastel-cli): create CLI POCs
    
    * fix(cli, pastel-cli): fix formatting
    
    * refactor(biome): update biome config
    
    * feat(pastel-cli): make Pastel POC more relevant
    
    make Pastel POC produce more relevant output
    
    * refactor(pastel-cli): use Static for verbose output"
    
    * refactor(cli): make Commander POC more relevant
    
    * fix(pastel-cli): fix formatting
    
    * chore: remove eslint plugins
    
    * chore(biome): remove biome rule override
    
    * feat(pastel-cli, cli): move Pastel to CLI dir
    
    * fix(biome): adjust biome config
    
    biome config adjustment after moving Pastel app to /cli dir
    
    * chore(cli/tsconfig): remove duplicated props from tsconfig

---
+++ packages/cli/src/commands/index.tsx
@@ -0,0 +1,93 @@
+import { Box, Static, Text } from "ink";
+import React, { useState, useEffect } from "react";
+import zod from "zod";
+import Spinner from "../components/spinner.js";
+import { licenses } from "../mocks.js";
+
+export const options = zod.object({
+  verbose: zod.boolean().default(false).describe("Verbose output"),
+});
+
+type Props = {
+  options: zod.infer<typeof options>;
+};
+
+export default function Index({ options }: Props) {
+  const [working, setWorking] = useState(true);
+
+  const [processed, setProcessed] = useState<
+    {
+      modulePath: string;
+      license: string;
+      licensePath: string;
+      error: boolean;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    setWorking(true);
+
+    for (const license of licenses) {
+      setTimeout(
+        () =>
+          setProcessed((existing) => [
+            ...existing,
+            {
+              modulePath: license.modulePath,
+              license: license.license,
+              licensePath: license.licensePath,
+              error: license.license !== "MIT",
+            },
+          ]),
+        500,
+      );
+    }
+
+    setWorking(false);
+  }, []);
+
+  if (working && !options.verbose) {
+    return (
+      <Box>
+        <Spinner />
+        <Text>Processing licenses...</Text>
+      </Box>
+    );
+  }
+
+  return (
+    <Box flexDirection="column">
+      {options.verbose && (
+        <Static items={processed}>
+          {(item) => (
+            <Box
+              key={item.modulePath}
+              flexDirection="column"
+              borderStyle="single"
+              borderColor={item.error ? "red" : "white"}
+            >
+              <Text>Module path: {item.modulePath}</Text>
+              <Text>License: {item.license}</Text>
+              <Text>License path: {item.licensePath}</Text>
+            </Box>
+          )}
+        </Static>
+      )}
+      <Box flexDirection="column" borderStyle="single" borderColor="white">
+        <Box>
+          <Text>Licenses found: {processed.length}</Text>
+        </Box>
+        <Box>
+          <Text>
+            Valid licenses: {processed.filter((item) => !item.error).length}
+          </Text>
+        </Box>
+        <Box>
+          <Text color="red">
+            Prohibited licenses: {processed.filter((item) => item.error).length}
+          </Text>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
