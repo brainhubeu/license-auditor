32f8b5ae1b385e86ceb7e565f6f6924555adb1a5
Filip Kublin <filip.kublin@brainhub.pl>
2024-10-15 12:30:57 +0200

    fix(cli, package.json): use exec instead of spawn to install license-auditor

--- packages/cli/bin/index.cjs
+++ packages/cli/bin/index.cjs
@@ -1,12 +1,11 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const { spawn } = require("node:child_process");
+const { exec } = require("node:child_process");
 
 const currentDir = process.cwd();
-const licenseDir = path.resolve(currentDir, 'licenses');
+const licenseDir = path.resolve(currentDir, "licenses");
 
-
-const usePredefinedLists = process.argv[2] === '--use-default';
+const usePredefinedLists = process.argv[2] === "--use-default";
 
 fs.mkdirSync(licenseDir, { recursive: true });
 
@@ -15,6 +14,11 @@ const templateDir = usePredefinedLists
   : path.resolve(__dirname, "template/blank");
 fs.cpSync(templateDir, licenseDir, { recursive: true });
 
-spawn("npm", ["install"," license-auditor"]);
+exec("npm install @brainhubeu/license-auditor", (error) => {
+  if (error) {
+    console.error(`Error installing package: ${error.message}`);
+    return;
+  }
 
-console.log("Success! Configured licenses for license-auditor.");
+  console.log("Success! Configured licenses for license-auditor.");
+});
---
+++ packages/cli/licenses/whitelist.js
@@ -0,0 +1,4 @@
+/* Provide a list of whitelisted licenses for the project below */
+const whitelist = [];
+
+module.exports = whitelist;
