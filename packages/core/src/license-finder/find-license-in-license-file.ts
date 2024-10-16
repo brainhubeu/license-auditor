68aa20c52642b90866571ecd60de1acb6ef0d701
Filip Kublin <filip.kublin@brainhub.pl>
2024-10-16 14:46:56 +0200

    refactor(core): compare license content against available licenses

--- packages/core/src/license-finder/find-license-in-license-file.ts
+++ packages/core/src/license-finder/find-license-in-license-file.ts
@@ -1,43 +1,30 @@
 import * as fs from "node:fs";
 import { licenses } from "../license/licenses";
 
-const licenseMap: Record<string, string> = {
-  "(The MIT License)": "MIT",
-  "Apache License": "Apache",
-  "ISC License": "MIT",
-  "MIT License": "MIT",
-  "The ISC License": "ISC",
-  "The MIT License (MIT)": "MIT",
-  "The MIT License (MIT)^M": "MIT",
-  "The MIT License": "MIT",
-  "This software is released under the MIT license:": "MIT",
-};
-
 const templates = {
   [fs.readFileSync(`${__dirname}/templates/BSD-2-Clause.txt`).toString()]:
     "BSD 2-Clause",
   [fs.readFileSync(`${__dirname}/templates/MIT.txt`).toString()]: "MIT",
 };
 
-function retrieveLicenseFromLicenseFileContent(content: string): string {
+function retrieveLicenseFromLicenseFileContent(
+  content: string
+): string | string[] {
   const lines = content.split("\n");
-  const license = lines.find((line) => /license/i.test(line));
 
-  if (!license) {
-    return "";
+  const licenseArr = licenses.filter((license) => content.includes(license));
+
+  if (!licenseArr.length) {
+    return content;
   }
-  const mapped = licenseMap[license.trim()];
-  if (mapped) {
-    mapped;
-  }
+
   const withoutFirstLine = lines
     .slice(1)
     .filter((line) => line.length)
     .join("\n");
-  return templates[withoutFirstLine] || content;
+  return templates[withoutFirstLine] || licenseArr;
 }
 
-// todo: re-implement logic responsible for searching for license in license files
 export function findLicenseInLicenseFile(
   filename: string
 ): LicenseWithPath | undefined {
@@ -45,12 +32,12 @@ export function findLicenseInLicenseFile(
     return;
   }
   const content = fs.readFileSync(filename).toString();
-  const foundLicense = retrieveLicenseFromLicenseFileContent(content);
+  const foundLicenses = retrieveLicenseFromLicenseFileContent(content);
 
-  const matchedLicense = licenses.find((license) => license === foundLicense);
+  const matchedLicense = licenses.find((license) => license === foundLicenses);
 
   if (!matchedLicense) {
-    return;
+    return { license: "Unlicense", licensePath: filename };
   }
 
   return { license: matchedLicense, licensePath: filename };
