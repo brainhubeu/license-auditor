d457d19e56359eb3dd2646218b03f51b39fcf3be
Filip Kublin <filip.kublin@brainhub.pl>
2024-10-18 10:13:38 +0200

    refactor(cli): deduplicate code

--- packages/cli/src/bin/index.ts
+++ packages/cli/src/bin/index.ts
@@ -1,31 +1,5 @@
-import fs from "node:fs";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { executeConfig } from "../utils/execute-config.js";
 
-try {
-  const currentDir = process.cwd();
-  const usePredefinedLists = process.argv[2] === "--use-default";
+const usePredefinedLists = process.argv[2] === "--use-default";
 
-  fs.mkdirSync(currentDir, { recursive: true });
-
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-
-  const templateDir = usePredefinedLists
-    ? path.resolve(__dirname, "template/filled")
-    : path.resolve(__dirname, "template/blank");
-  fs.cpSync(templateDir, currentDir, { recursive: true });
-
-  console.log("Success!");
-  if (usePredefinedLists) {
-    console.log(
-      `Created a default license list for license-auditor at ${currentDir}/license-auditor.config.js`,
-    );
-  } else {
-    console.log(
-      `Created a blank license list for license-auditor at ${currentDir}/license-auditor.config.js`,
-    );
-  }
-} catch (err) {
-  console.error("Failed to complete license configuration: ", err);
-}
+executeConfig(usePredefinedLists);
