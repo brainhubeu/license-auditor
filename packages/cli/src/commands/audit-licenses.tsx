9824db6d4448dc3d51209876525bf389926c618b
Angela <angela@brainhub.pl>
2024-10-29 11:10:16 +0100

    chore: lint

--- packages/cli/src/commands/audit-licenses.tsx
+++ packages/cli/src/commands/audit-licenses.tsx
@@ -2,7 +2,7 @@ import { auditLicenses } from "@brainhubeu/license-auditor-core";
 import { ConfigSchema, type LicenseAuditResult } from "@license-auditor/data";
 import { Box, Text, useApp } from "ink";
 import { useEffect, useState } from "react";
-import type { z } from "zod";
+import { z } from "zod";
 import AuditResult from "../components/audit-result.js";
 import { SpinnerWithLabel } from "../components/spinner-with-label.js";
 import { cliOptions } from "../options.js";
@@ -15,6 +15,11 @@ export type AuditLicensesOptions = {
   options: z.infer<typeof auditLicensesOptions>;
 };
 
+const envSchema = z.object({
+  // biome-ignore lint/style/useNamingConvention: this is a constant
+  ROOT_DIR: z.string().min(1).default(process.cwd()),
+});
+
 export default function AuditLicenses({ options }: AuditLicensesOptions) {
   const [working, setWorking] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -26,8 +31,13 @@ export default function AuditLicenses({ options }: AuditLicensesOptions) {
 
     const getResults = async () => {
       try {
+        const parsedEnv = envSchema.safeParse(process.env);
+        if (parsedEnv.error) {
+          setError(parsedEnv.error.message);
+          return;
+        }
         const result = await auditLicenses(
-          process.env["ROOT_DIR"] ?? process.cwd(),
+          parsedEnv.data.ROOT_DIR,
           options.config,
         );
         console.log("Result:", result);
--- packages/core/src/license-finder/find-license.ts
+++ packages/core/src/license-finder/find-license.ts
@@ -1,8 +1,8 @@
 import * as path from "node:path";
+import type { PackageJsonType } from "../file-utils.js";
 import { parseLicenseFiles } from "./find-license-in-license-file.js";
 import { findLicenseInPackageJson } from "./find-license-in-package-json.js";
 import type { LicensesWithPath } from "./licenses-with-path.js";
-import type { PackageJsonType } from "../file-utils.js";
 
 export async function findLicenses(
   packageJson: PackageJsonType,
