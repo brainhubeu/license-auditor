e7d7226576151bb0c9a196dbf364d9bad3c66e4d
janzon <jan@brainhub.pl>
2024-11-13 13:10:21 +0100

    feat: added a --json option that writes report to the json file
    When `--json` option has no value, the result will be save to `license-auditor.results.json` file.
    If the provided path points to a directory, a `license-auditor.results.json` file with a report will be created in the specified directory

--- packages/cli/src/commands/index.tsx
+++ packages/cli/src/commands/index.tsx
@@ -3,13 +3,21 @@ import { Box, Text } from "ink";
 import Spinner from "ink-spinner";
 import { z } from "zod";
 import AuditLicenses from "../components/audit-licenses/audit-licenses.js";
+import { JSON_RESULT_FILE_NAME } from "../constants/options-constants.js";
 import { useReadConfiguration } from "../hooks/use-read-config-file.js";
+import { useValidateJsonPath } from "../hooks/use-validate-json-path.js";
 
 export const options = z.object({
   verbose: z.boolean().default(false).describe("Verbose output"),
   filter: LicenseStatusSchema.optional().describe(
     "Filter by license status - whitelist, blacklist, or unknown",
   ),
+  json: z
+    .union([z.string(), z.boolean()])
+    .optional()
+    .describe(
+      `Save the result to a JSON file. If no path is not provided, a file named ${JSON_RESULT_FILE_NAME} will be created in the current directory.`,
+    ),
 });
 
 export type Options = {
@@ -18,15 +26,17 @@ export type Options = {
 
 export default function Index({ options }: Options) {
   const { configFile } = useReadConfiguration();
+  const validateJsonResult = useValidateJsonPath(options.json);
 
   // todo: handle errors thrown in readConfiguration - prompt the user accordingly
   // for now let's assume all is well and the file's been found
-  if (configFile?.config) {
+  if (configFile?.config && validateJsonResult.validated) {
     return (
       <AuditLicenses
         verbose={options.verbose}
         config={configFile.config}
         filter={options.filter}
+        json={validateJsonResult.path}
       />
     );
   }
