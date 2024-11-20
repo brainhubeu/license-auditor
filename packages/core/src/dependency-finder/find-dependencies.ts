a1e5dda7020111b7520c7102aebb1f59b31ad1a0
jan-zon-brainhub <156670447+jan-zon-brainhub@users.noreply.github.com>
2024-11-20 15:11:19 +0100

    feat: added --production flag to ignore development dependencies (#84)

--- packages/core/src/dependency-finder/find-dependencies.ts
+++ packages/core/src/dependency-finder/find-dependencies.ts
@@ -4,12 +4,17 @@ import { findNpmDependencies } from "./npm.js";
 import { findPnpmDependencies } from "./pnpm.js";
 import { findYarnClassicDependencies } from "./yarn-classic.js";
 
-export async function findDependencies(
-  packageManager: SupportedPm,
-  projectRoot: string,
-): Promise<string[]> {
+export async function findDependencies({
+  packageManager,
+  projectRoot,
+  production,
+}: {
+  packageManager: SupportedPm;
+  projectRoot: string;
+  production?: boolean | undefined;
+}): Promise<string[]> {
   const [dependencies, internalPackages] = await Promise.all([
-    findExternalDependencies(packageManager, projectRoot),
+    findExternalDependencies({ packageManager, projectRoot, production }),
     findInternalPackages(projectRoot),
   ]);
 
@@ -18,17 +23,22 @@ export async function findDependencies(
   );
 }
 
-function findExternalDependencies(
-  packageManager: SupportedPm,
-  projectRoot: string,
-): Promise<string[]> {
+function findExternalDependencies({
+  packageManager,
+  projectRoot,
+  production,
+}: {
+  packageManager: SupportedPm;
+  projectRoot: string;
+  production?: boolean | undefined;
+}): Promise<string[]> {
   switch (packageManager) {
     case "npm":
-      return findNpmDependencies(projectRoot);
+      return findNpmDependencies(projectRoot, production);
     case "pnpm":
-      return findPnpmDependencies(projectRoot);
+      return findPnpmDependencies(projectRoot, production);
     case "yarn-classic":
-      return findYarnClassicDependencies(projectRoot);
+      return findYarnClassicDependencies(projectRoot, production);
     default:
       throw new Error("Unsupported package manager");
   }
