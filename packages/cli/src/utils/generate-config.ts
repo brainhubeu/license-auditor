5980452cc7ff4a09555fa5a19de36f49c31413da
Piotr Walen <piotr.walen@brainhub.pl>
2024-11-20 14:06:19 +0100

    refactor(core,cli): add custom exceptions

--- packages/cli/src/utils/generate-config.ts
+++ packages/cli/src/utils/generate-config.ts
@@ -1,6 +1,7 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import { GenerateConfigException } from "@brainhubeu/license-auditor-core";
 import type { ConfigExtension } from "../constants/config-constants.js";
 
 export enum ConfigListType {
@@ -35,10 +36,12 @@ export async function generateConfig(
     await copyConfigFile(dir, configListType, extension);
 
     return `Configured license-auditor with ${configListType} license whitelist and blacklist at: ${dir}/license-auditor.config${extension}`;
-  } catch (err) {
-    console.log(err);
-    // todo: proper error handling
-    // this is temporary and lacks actual guidance on how to resolve the issue
-    throw new Error("Failed to complete license configuration");
+  } catch (error) {
+    throw new GenerateConfigException(
+      "Failed to complete license configuration",
+      {
+        originalError: error,
+      },
+    );
   }
 }
