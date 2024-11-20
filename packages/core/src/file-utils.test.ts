ab15ca7eefa045a2335ca8540aae3e55d43722ee
janzon <jan@brainhub.pl>
2024-11-20 15:03:24 +0100

    feat: added vitest with unit tests for packageJsonSchema

---
+++ packages/core/src/file-utils.test.ts
@@ -0,0 +1,85 @@
+import { describe, expect, test } from "vitest";
+
+import { packageJsonSchema } from "./file-utils.js";
+
+describe("packageJsonSchema", () => {
+  describe("should validate correctly", () => {
+    test("when there are no license nor licenses", () => {
+      expect(packageJsonSchema.safeParse({ name: "valid" }).success).toBe(true);
+    });
+    test("when license is valid string", () => {
+      expect(
+        packageJsonSchema.safeParse({ name: "valid", license: "MIT" }).success,
+      ).toBe(true);
+    });
+    test("when license is valid object", () => {
+      expect(
+        packageJsonSchema.safeParse({
+          name: "valid",
+          license: { type: "MIT", url: "https://mit.edu" },
+        }).success,
+      ).toBe(true);
+    });
+    test("when licenses are array of string", () => {
+      expect(
+        packageJsonSchema.safeParse({ name: "valid", licenses: ["MIT"] })
+          .success,
+      ).toBe(true);
+    });
+
+    test("when licenses are array of object", () => {
+      expect(
+        packageJsonSchema.safeParse({
+          name: "valid",
+          licenses: [{ type: "MIT", url: "https://mit.edu" }],
+        }).success,
+      ).toBe(true);
+    });
+
+    test("when license is valid and licenses are not valid", () => {
+      expect(
+        packageJsonSchema.safeParse({
+          name: "valid",
+          license: "MIT",
+          licenses: [400],
+        }).success,
+      ).toBe(true);
+    });
+
+    test("when license is not valid and licenses are valid", () => {
+      expect(
+        packageJsonSchema.safeParse({
+          name: "valid",
+          license: 400,
+          licenses: ["MIT"],
+        }).success,
+      ).toBe(true);
+    });
+  });
+
+  describe("should fail", () => {
+    test("when license is not valid and licenses are missing", () => {
+      console.log(
+        packageJsonSchema.safeParse({ name: "invalid", license: 400 }),
+      );
+      expect(
+        packageJsonSchema.safeParse({ name: "invalid", license: 400 }).success,
+      ).toBe(false);
+    });
+    test("when licenses are invalid and license is missing", () => {
+      expect(
+        packageJsonSchema.safeParse({ name: "invalid", licenses: [400] })
+          .success,
+      ).toBe(false);
+    });
+    test("when both license and licenses are invalid", () => {
+      expect(
+        packageJsonSchema.safeParse({
+          name: "invalid",
+          license: 400,
+          licenses: [400],
+        }).success,
+      ).toBe(false);
+    });
+  });
+});
