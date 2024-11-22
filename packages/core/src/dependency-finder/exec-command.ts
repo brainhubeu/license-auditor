b7740565ee36e450745ec32a6f8cdb0354a4c9b9
Filip Kublin <filip.kublin@brainhub.pl>
2024-11-22 15:14:31 +0100

    revert(core): restore exec-command code

--- packages/core/src/dependency-finder/exec-command.ts
+++ packages/core/src/dependency-finder/exec-command.ts
@@ -1,26 +1,22 @@
 import { exec } from "node:child_process";
-import { promisify } from "node:util";
-
-import { ExecCommandException } from "../exceptions/index.js";
-
-const execAsync = promisify(exec);
+import { CommandExecutionError } from "../errors.js";
 
 export async function execCommand(
   command: string,
   cwd: string,
 ): Promise<string> {
-  try {
-    const { stdout } = await execAsync(command, { cwd });
-    return stdout;
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new ExecCommandException(
-        `Error executing command ${command}: ${error.name} - ${error.message}`,
-        { originalError: error },
-      );
-    }
-    throw new ExecCommandException(`Error executing command ${command}`, {
-      originalError: error,
+  return new Promise((resolve, reject) => {
+    exec(command, { cwd }, (_, stdout, stderr) => {
+      if (stderr) {
+        reject(
+          new CommandExecutionError(
+            `Command "${command}" returned error.`,
+            stdout,
+            stderr,
+          ),
+        );
+      }
+      resolve(stdout);
     });
-  }
+  });
 }
