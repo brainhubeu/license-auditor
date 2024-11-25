7d7861fcf2cf4acae0ebb51d2ddf8e0d6d5a6b05
“pawelkoniecznybh” <pawel.konieczny@brainhub.pl>
2024-11-25 08:57:48 +0100

    fix: format write

--- packages/cli/src/components/audit-licenses/including-unknown-result.tsx
+++ packages/cli/src/components/audit-licenses/including-unknown-result.tsx
@@ -2,53 +2,53 @@ import type { LicenseAuditResult } from "@license-auditor/data";
 import { Box } from "ink";
 import LicenseList from "./license-list.js";
 import {
-	BlacklistedMessage,
-	CompliantMessage,
-	FailureHeaderMessage,
-	UnknownMessage,
-	WarningHeaderMessage,
+  BlacklistedMessage,
+  CompliantMessage,
+  FailureHeaderMessage,
+  UnknownMessage,
+  WarningHeaderMessage,
 } from "./result-messages.js";
 
 export default function IncludingUnknownResult({
-	groupedByStatus,
-	verbose,
+  groupedByStatus,
+  verbose,
 }: Omit<
-	LicenseAuditResult,
-	"notFound" | "needsUserVerification" | "overrides"
+  LicenseAuditResult,
+  "notFound" | "needsUserVerification" | "overrides"
 > & {
-	verbose: boolean;
+  verbose: boolean;
 }) {
-	const hasWhitelisted = groupedByStatus.whitelist.length > 0;
-	const hasBlacklisted = groupedByStatus.blacklist.length > 0;
+  const hasWhitelisted = groupedByStatus.whitelist.length > 0;
+  const hasBlacklisted = groupedByStatus.blacklist.length > 0;
 
-	return (
-		<Box flexDirection="column">
-			<Box marginBottom={1}>
-				{hasBlacklisted ? <FailureHeaderMessage /> : <WarningHeaderMessage />}
-			</Box>
-			{hasWhitelisted && (
-				<Box>
-					<CompliantMessage count={groupedByStatus.whitelist.length} />
-				</Box>
-			)}
-			{hasBlacklisted && (
-				<>
-					<Box>
-						<BlacklistedMessage count={groupedByStatus.blacklist.length} />
-					</Box>
-					<LicenseList
-						detectedLicenses={groupedByStatus.blacklist}
-						verbose={verbose}
-					/>
-				</>
-			)}
-			<Box>
-				<UnknownMessage count={groupedByStatus.unknown.length} />
-			</Box>
-			<LicenseList
-				detectedLicenses={groupedByStatus.unknown}
-				verbose={verbose}
-			/>
-		</Box>
-	);
+  return (
+    <Box flexDirection="column">
+      <Box marginBottom={1}>
+        {hasBlacklisted ? <FailureHeaderMessage /> : <WarningHeaderMessage />}
+      </Box>
+      {hasWhitelisted && (
+        <Box>
+          <CompliantMessage count={groupedByStatus.whitelist.length} />
+        </Box>
+      )}
+      {hasBlacklisted && (
+        <>
+          <Box>
+            <BlacklistedMessage count={groupedByStatus.blacklist.length} />
+          </Box>
+          <LicenseList
+            detectedLicenses={groupedByStatus.blacklist}
+            verbose={verbose}
+          />
+        </>
+      )}
+      <Box>
+        <UnknownMessage count={groupedByStatus.unknown.length} />
+      </Box>
+      <LicenseList
+        detectedLicenses={groupedByStatus.unknown}
+        verbose={verbose}
+      />
+    </Box>
+  );
 }
--- packages/cli/src/components/audit-licenses/not-found-result.tsx
+++ packages/cli/src/components/audit-licenses/not-found-result.tsx
@@ -3,42 +3,42 @@ import figures from "figures";
 import { Box, Text } from "ink";
 
 export default function NotFoundResult({
-	notFound,
-	verbose,
+  notFound,
+  verbose,
 }: Omit<
-	LicenseAuditResult,
-	"needsUserVerification" | "groupedByStatus" | "overrides"
+  LicenseAuditResult,
+  "needsUserVerification" | "groupedByStatus" | "overrides"
 > & {
-	verbose: boolean;
+  verbose: boolean;
 }) {
-	const describePackagesCount =
-		notFound.size === 1 ? "package is" : "packages are";
+  const describePackagesCount =
+    notFound.size === 1 ? "package is" : "packages are";
 
-	return (
-		<Box flexDirection="column">
-			<Box>
-				<Text color="yellow">{figures.warning}</Text>
-				<Text>
-					{notFound.size} {describePackagesCount} missing license information:
-				</Text>
-			</Box>
-			<Box flexDirection="column" marginLeft={2}>
-				{Array.from(notFound).map(
-					([packageName, { packagePath, errorMessage }]) => (
-						<Box key={packagePath} marginBottom={verbose ? 1 : 0}>
-							<Text color="gray">{figures.pointerSmall}</Text>
-							{verbose ? (
-								<Text>
-									{" "}
-									{packageName}: {packagePath} Error: {errorMessage}
-								</Text>
-							) : (
-								<Text> {packageName} </Text>
-							)}
-						</Box>
-					),
-				)}
-			</Box>
-		</Box>
-	);
+  return (
+    <Box flexDirection="column">
+      <Box>
+        <Text color="yellow">{figures.warning}</Text>
+        <Text>
+          {notFound.size} {describePackagesCount} missing license information:
+        </Text>
+      </Box>
+      <Box flexDirection="column" marginLeft={2}>
+        {Array.from(notFound).map(
+          ([packageName, { packagePath, errorMessage }]) => (
+            <Box key={packagePath} marginBottom={verbose ? 1 : 0}>
+              <Text color="gray">{figures.pointerSmall}</Text>
+              {verbose ? (
+                <Text>
+                  {" "}
+                  {packageName}: {packagePath} Error: {errorMessage}
+                </Text>
+              ) : (
+                <Text> {packageName} </Text>
+              )}
+            </Box>
+          ),
+        )}
+      </Box>
+    </Box>
+  );
 }
