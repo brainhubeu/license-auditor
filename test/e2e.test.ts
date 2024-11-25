1c87d37ad3ebae2f51ac4de6ca8df2b5d736d48f
“pawelkoniecznybh” <pawel.konieczny@brainhub.pl>
2024-11-25 13:02:06 +0100

    feat: add e2e and unit test for finding license in file

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -47,7 +47,7 @@ describe("license-auditor", () => {
         force: true,
       });
       await execAsync("npm i --legacy-peer-deps", {
-        cwd: path.resolve(__dirname, "testProject"),
+        cwd: context.testDirectory,
       });
 
       const { output, errorCode } = await runCliCommand({
@@ -58,6 +58,124 @@ describe("license-auditor", () => {
 
       expect(errorCode).toBe(0);
       expect(output).toContain("Results incomplete because of an error.");
+    });
+  });
+
+  describe("parse license files", () => {
+    test("parse license files: single license file with correct license", async (context: TestContext) => {
+      await addPackage(
+        context.testDirectory,
+        "node_modules/testing-license-file",
+        {
+          version: "1.0.0",
+        },
+        [{ name: "LICENSE-MIT", content: "MIT" }],
+      );
+
+      const { output, errorCode } = await runCliCommand({
+        command: "npx",
+        args: [getCliPath()],
+        cwd: context.testDirectory,
+      });
+
+      expect(errorCode).toBe(0);
+      expect(output).toContain("247 licenses are compliant");
+    });
+
+    test("parse license files: one correct license file and one incorrect license file", async (context: TestContext) => {
+      await addPackage(
+        context.testDirectory,
+        "node_modules/testing-license-file",
+        {
+          version: "1.0.0",
+        },
+        [
+          { name: "LICENSE-MIT", content: "MIT" },
+          { name: "LICENSE-WRONG", content: "WRONG" },
+        ],
+      );
+
+      const { output, errorCode } = await runCliCommand({
+        command: "npx",
+        args: [getCliPath(), "--verbose"],
+        cwd: context.testDirectory,
+      });
+
+      expect(errorCode).toBe(0);
+      expect(output).toContain("1 package is requiring manual checking");
+      expect(output).toContain(
+        "We found some, but not all licenses for package",
+      );
+    });
+
+    test("parse license files: single license file with incorrect license", async (context: TestContext) => {
+      await addPackage(
+        context.testDirectory,
+        "node_modules/testing-license-file",
+        {
+          version: "1.0.0",
+        },
+        [{ name: "LICENSE-WRONG", content: "WRONG" }],
+      );
+
+      const { output, errorCode } = await runCliCommand({
+        command: "npx",
+        args: [getCliPath(), "--verbose"],
+        cwd: context.testDirectory,
+      });
+
+      expect(errorCode).toBe(0);
+      expect(output).toContain("1 package is requiring manual checking");
+      expect(output).toContain(
+        "We’ve found a license file, but no matching licenses in",
+      );
+    });
+
+    test("parse license files: two license files with correct licenses", async (context: TestContext) => {
+      await addPackage(
+        context.testDirectory,
+        "node_modules/testing-license-file",
+        {
+          version: "1.0.0",
+        },
+        [
+          { name: "LICENSE-MIT", content: "MIT" },
+          { name: "LICENSE-ISC", content: "ISC" },
+        ],
+      );
+
+      const { output, errorCode } = await runCliCommand({
+        command: "npx",
+        args: [getCliPath()],
+        cwd: context.testDirectory,
+      });
+
+      expect(errorCode).toBe(0);
+      expect(output).toContain("247 licenses are compliant");
+    });
+
+    test("parse license files: two license files, one correct and one not whitelisted", async (context: TestContext) => {
+      await addPackage(
+        context.testDirectory,
+        "node_modules/testing-license-file",
+        {
+          version: "1.0.0",
+        },
+        [
+          { name: "LICENSE-MIT", content: "MIT" },
+          { name: "LICENSE-AAL", content: "AAL" },
+        ],
+      );
+
+      const { output, errorCode } = await runCliCommand({
+        command: "npx",
+        args: [getCliPath(), "--verbose"],
+        cwd: context.testDirectory,
+      });
+
+      expect(errorCode).toBe(0);
+      expect(output).toContain("1 package is requiring manual checking");
+      expect(output).toContain("Not all licenses are whitelisted");
     });
   });
 });
