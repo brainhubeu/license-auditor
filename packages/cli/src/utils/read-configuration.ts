f8f10967d3edd87632a804bbaeab7a597fd30e04
janzon <jan@brainhub.pl>
2024-11-27 12:52:08 +0100

    fix: fix loading ts config file

--- packages/cli/src/utils/read-configuration.ts
+++ packages/cli/src/utils/read-configuration.ts
@@ -1,9 +1,14 @@
 import { cosmiconfig } from "cosmiconfig";
+import { TypeScriptLoader } from "cosmiconfig-typescript-loader";
 import { MODULE_NAME } from "../constants/config-constants.js";
 import { envSchema } from "../env.js";
 
 async function readConfiguration() {
-  const explorer = cosmiconfig(MODULE_NAME);
+  const explorer = cosmiconfig(MODULE_NAME, {
+    loaders: {
+      ".ts": TypeScriptLoader(),
+    },
+  });
 
   const parsedEnv = envSchema.safeParse(process.env);
 
