71528ca043e1174b6d1b8bb295915bd4412aba34
Mateusz Jarzębowski-Bownik <mateuszjarzebowskibownik@Mateuszs-MacBook-Pro.local>
2024-12-03 09:50:17 +0100

    docs(core): add comment explaining silencing a common error

--- packages/core/src/license-finder/parse-license-logical-expression.ts
+++ packages/core/src/license-finder/parse-license-logical-expression.ts
@@ -1,12 +1,14 @@
 import parse, { type Info } from "spdx-expression-parse";
 
 export function parseLicenseLogicalExpression(
-  licenseExpression: unknown,
+  licenseExpression: unknown
 ): Info | undefined {
   if (typeof licenseExpression === "string") {
     try {
       return parse(licenseExpression);
     } catch (error) {
+      // Most common error when spdx-expression-parse fails to parse the license expression.
+      // The error message thrown is not very useful, hence we silence it.
       if (!(error instanceof Error && error.message.startsWith("Unexpected"))) {
         console.error(error);
       }
