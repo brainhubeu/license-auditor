4219155868cb7032ecc3229d1a84585560e7ba66
“pawelkoniecznybh” <pawel.konieczny@brainhub.pl>
2024-12-09 10:29:51 +0100

    feat: add exit codes to some catch statements

--- packages/cli/src/components/config-error-handler.tsx
+++ packages/cli/src/components/config-error-handler.tsx
@@ -4,55 +4,56 @@ import { useState } from "react";
 import Init from "../commands/init.js";
 import type { SelectItem } from "../constants/select-constants.js";
 import {
-  ReadConfigErrorType,
-  type ReadConfigurationError,
+	ReadConfigErrorType,
+	type ReadConfigurationError,
 } from "../hooks/use-read-config-file.js";
 
 interface ReadConfigurationErrorProps {
-  error: ReadConfigurationError;
+	error: ReadConfigurationError;
 }
 
 export const booleanSelectItems: SelectItem<boolean>[] = [
-  {
-    label: "Yes",
-    value: true,
-  },
-  {
-    label: "No",
-    value: false,
-  },
+	{
+		label: "Yes",
+		value: true,
+	},
+	{
+		label: "No",
+		value: false,
+	},
 ] as const;
 
 export function ConfigErrorHandler({ error }: ReadConfigurationErrorProps) {
-  switch (error.type) {
-    case ReadConfigErrorType.NotFound:
-      return <ConfigFileNotFoundHandler message={error.message} />;
-    default:
-      return <Text>{error.message}</Text>;
-  }
+	process.exitCode = 1;
+	switch (error.type) {
+		case ReadConfigErrorType.NotFound:
+			return <ConfigFileNotFoundHandler message={error.message} />;
+		default:
+			return <Text>{error.message}</Text>;
+	}
 }
 
 function ConfigFileNotFoundHandler({ message }: { message: string }) {
-  const { exit } = useApp();
-  const [shouldCreateConfig, setShouldCreateConfig] = useState(false);
+	const { exit } = useApp();
+	const [shouldCreateConfig, setShouldCreateConfig] = useState(false);
 
-  if (shouldCreateConfig) {
-    return <Init />;
-  }
+	if (shouldCreateConfig) {
+		return <Init />;
+	}
 
-  return (
-    <Box flexDirection="column">
-      <Text>{message}</Text>
-      <Text>Would you like to create a configuration file now? (Y/n)</Text>
-      <SelectInput
-        items={booleanSelectItems}
-        onSelect={(item) => {
-          if (!item.value) {
-            exit();
-          }
-          setShouldCreateConfig(item.value);
-        }}
-      />
-    </Box>
-  );
+	return (
+		<Box flexDirection="column">
+			<Text>{message}</Text>
+			<Text>Would you like to create a configuration file now? (Y/n)</Text>
+			<SelectInput
+				items={booleanSelectItems}
+				onSelect={(item) => {
+					if (!item.value) {
+						exit();
+					}
+					setShouldCreateConfig(item.value);
+				}}
+			/>
+		</Box>
+	);
 }
