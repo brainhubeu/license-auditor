ebe79f3bd7ecde2cbdf5a1f1c9ae3c5031a9d1f5
Marcin <marcin.zmudka@brainhub.pl>
2025-02-10 17:22:55 +0100

    fix(workflow): change vite url of landing page

--- apps/landing/vite.config.ts
+++ apps/landing/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/license-auditor/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
